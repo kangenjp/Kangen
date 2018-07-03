da032478225d97d3f058c08d22b236ae9795781f
@hi_karuh <mail.kangen.jp@gmail.com>
2018-07-03 22:45:21 +0900

    :sparkles: UpdateFiles!
    main builded file is  include on "docs" path.
    Supported Github Pages.

---
+++ docs/precache-manifest.272fd44c6a6435f915d9319fbac8c42e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "81c408da79b86cdc0f23",
+    "url": "/js/chunk-vendors.81c408da.js"
+  },
+  {
+    "revision": "4b70c1000fbf8bc59d58",
+    "url": "/js/app.4b70c100.js"
+  },
+  {
+    "revision": "721829b4687bc9632731787f3eaa7bd8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81c408da79b86cdc0f23",
+    "url": "/css/chunk-vendors.268c6fa8.css"
+  },
+  {
+    "revision": "4b70c1000fbf8bc59d58",
+    "url": "/css/app.1515f46f.css"
+  }
+];
