8abad7ec12fabe50a7a43dedc3ca3639844dee64
@hi_karuh <mail.kangen.jp@gmail.com>
2018-07-03 18:15:51 +0900

    :sparkles: First Commit!
    とりあえずbuildしたものをcommit

---
+++ precache-manifest.6116aff91e6fc99412d7501537c59c70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "81c408da79b86cdc0f23",
+    "url": "/js/chunk-vendors.81c408da.js"
+  },
+  {
+    "revision": "f37f941bd110cec7fcb3",
+    "url": "/js/app.f37f941b.js"
+  },
+  {
+    "revision": "ae34245dc1b4a490f8220ecb31c9e609",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81c408da79b86cdc0f23",
+    "url": "/css/chunk-vendors.268c6fa8.css"
+  },
+  {
+    "revision": "f37f941bd110cec7fcb3",
+    "url": "/css/app.1515f46f.css"
+  }
+];
